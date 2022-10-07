6a04da2faada95e698abaa268b62c2253269bf58
Анна Хагнес <Anna@MacBook-Air-Anna.local>
2022-10-07 17:18:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data'
 import './App.css';
 
 function App() {
+
+  const [mountain, setMountain] = useState(0);
+  const { id, image, name, description, showMore } = data[mountain];
+
+  const [showText, setShowText] = useState(false);
+
+  console.log(data[mountain])
+
+  const previousMountain = () => {
+    setMountain((mountain => {
+      mountain --;
+
+      if (mountain < 0) {
+        return data.length -1;
+      }
+      return mountain;
+    }))
+  }
+
+  const nextMountain = () => {
+    setMountain((mountain => {
+      mountain ++;
+
+      if (mountain > data.length -1) {
+        mountain = 0;
+      }
+
+      return mountain;
+    }))
+  }
+  const showTextClick = (element) => {
+    element.showMore = !element.showMore 
+    setShowText(!showText)
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div> 
+      <div className="container">
+        <h1>My favorite places in Switzerland</h1> 
+      </div>
+      <div className="container">
+        <h2> {id}. {name}</h2>
+      </div>
+      <div className="container">
+        <img src={image} width="400px" alt="mountain" className="mountain" />
+      </div>
+      <div className="container">
+        <p> {showText ? description : description.substring(0, 150) + "..."}
+        <button onClick ={() => showTextClick(element)}> {showMore ? "Show less" : "Show more"}</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="btn container">
+        <button onClick = {previousMountain} className="mountain">previous</button>
+        <button onClick = {nextMountain} className="mountain">next</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+{
+    id: 1,
+    image: "https://images.unsplash.com/photo-1585951619576-6e961950cb60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8emVybWF0dHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60",
+    name: "Matterhorn",
+    description: "At 4,478 metres, the majestic Matterhorn – certainly the most famous mountain in Europe – looms over the breathtaking Alpine panorama. This Switzerland landmark is at the border of the western Swiss canton of Valais between Zermatt and the Italian resort of Breuil-Cervinia, and offers a paradise for all nature-lovers looking for peace and quiet. Its symmetric pyramid shape, the rocky tooth reaching the sky and the light reflections of the nearby Stellisee make the Matterhorn’s landscape a unique natural spectacle in Zermatt.",
+    showMore: false
+},
+{
+    id: 2,
+    image: "https://images.unsplash.com/photo-1628239019742-7f7e3b4ed077?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGVpZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    name: "Jungfrau",
+    description: "Breathtaking heights, idyllic lakes and unspoiled landscape: the many faces of the Jungfrau Region. The car-free villages of Wengen and Mürren are perfect to relax and unwind or to take part in the traditional processions. Activities happen at a faster pace on the First where visitors either take to the skies or explore the region with high speed.",
+    showMore: false
+},
+{
+    id: 3,
+    image: "https://images.unsplash.com/photo-1608200543295-e0ebb47d8aa5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGxhdXRlcmJydW5uZW58ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+    name: "Lauterbrunnen",
+    description: "Lauterbrunnen is situated in one of the most impressive trough valleys in the Alps, between gigantic rock faces and mountain peaks. With its 72 thundering waterfalls, secluded valleys, colourful alpine meadows and lonely mountain inns, the Lauterbrunnen Valley is one of the biggest nature conservation areas in Switzerland.",
+    showMore: false
+},
+{
+    id: 4,
+    image: "https://images.unsplash.com/photo-1546419793-1ee93d7cff56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGVpZ2VyJTIwbm9yZHdhbmR8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60",
+    name: "Eiger, Grindelwald",
+    description: "The Eiger is a 3,967-metre (13,015 ft) mountain of the Bernese Alps, overlooking Grindelwald and Lauterbrunnen in the Bernese Oberland of Switzerland, just north of the main watershed and border with Valais. It is the easternmost peak of a ridge crest that extends across the Mönch to the Jungfrau at 4,158 m (13,642 ft), constituting one of the most emblematic sights of the Swiss Alps",
+    showMore: false
+},
+{
+    id: 5,
+    image: "https://images.unsplash.com/photo-1537512786933-dda1dd51f47c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    name: "Furkapass",
+    description: "The Furka Pass links Andermatt in the Canton of Uri with Gletsch in the Canton of Valais. It is part of the popular Three-Pass-Ride over the Furka, Grimsel and Susten Pass. In summer, a ride with the nostalgic Furka steam engine from Realp to Gletsch is an unforgettable experience.",
+    showMore: false
+},
+]
